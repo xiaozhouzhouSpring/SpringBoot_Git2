55ae6cf8bc6b7ee81ff805088e34ae51c85e5959
xiaozhouzhouSpring <slhyz2@126.com>
2020-08-26 10:48:13 +0800

    第一次提交

---
+++ src/main/java/com/ygnet/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.ygnet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * HelloController
+ *
+ * @author shenhuz
+ * @date 2020/8/26
+ **/
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "Hello Worid";
+    }
+}
