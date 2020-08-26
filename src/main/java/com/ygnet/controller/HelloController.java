fc95d794c41bf5d755332a4818ce417917d1c495
xiaozhouzhouSpring <slhyz2@126.com>
2020-08-26 11:01:52 +0800

    增加jack接口

--- src/main/java/com/ygnet/controller/HelloController.java
+++ src/main/java/com/ygnet/controller/HelloController.java
@@ -15,4 +15,9 @@ public class HelloController {
     public String hello(){
         return  "Hello Worid";
     }
+
+    @RequestMapping("/jack")
+    public String jack(){
+        return  "Hello jack";
+    }
 }
